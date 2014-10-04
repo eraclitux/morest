a2ba174131241bd3f9bb42ef71270f1551ccfc15
eraclitux <eraclitux@gmail.com>
2014-10-04 15:59:04 +0200

    Move flags inside main

--- main.go
+++ main.go
@@ -12,18 +12,25 @@ import (
 	"net/http"
 )
 
-var mongoAddressFlag = flag.String("mongodb-address", "localhost", "Mongodb address. Can be a list of server in a cluster.")
-var tlsCertFlag = flag.String("ssl-cert", "", "Path to certificate file")
-var tlsKeyFlag = flag.String("ssl-key", "", "Path to key file")
-var portFlag = flag.Int("port", 9002, "Port to listen for requests.")
-var safeFlag = flag.Bool("safe-mode", true, "When false, MongoDB does not acknowledge the receipt of write operations. Faster but may lead to data loss.")
-
 func main() {
 	defer func() {
 		if r := recover(); r != nil {
 			log.Fatalf("An error occurred: %s", r)
 		}
 	}()
+	var tlsCertFlag = flag.String("ssl-cert", "", "Path to certificate file")
+	var tlsKeyFlag = flag.String("ssl-key", "", "Path to key file")
+	var portFlag = flag.Int("port", 9002, "Port to listen for requests.")
+	var mongoAddressFlag = flag.String(
+		"mongodb-address",
+		"localhost",
+		"Mongodb address. Can be a list of server in a cluster.",
+	)
+	var safeFlag = flag.Bool(
+		"safe-mode",
+		true,
+		"When false, MongoDB does not acknowledge the receipt of write operations. Faster but may lead to data loss.",
+	)
 	flag.Parse()
 	msession, err := mgo.Dial(*mongoAddressFlag)
 	if err != nil {
